13c7ce5d165ed3aec1ac0396514a21563c1c181e
Valentin Jubert <valentin.jubert@ideine.fr>
2020-03-07 12:28:57 +0100

    fix assembly name

--- Xmf2.Licences.iOS/Properties/AssemblyInfo.cs
+++ Xmf2.Licences.iOS/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.CompilerServices;
 // Information about this assembly is defined by the following attributes. 
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle("Xmf2.Licences.iOS")]
+[assembly: AssemblyTitle("Xmf2.Licences")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
